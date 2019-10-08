cef4c7c7a27691190f2557406be5e22dabfdf8dd
Luca Spagnolo <luca.spagnolo91@gmail.com>
2019-10-08 15:07:43 +0200

    troubleshooting google fonts on netlify by import css in header in nuxt config

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,8 +15,8 @@ export default {
   }
 }
 </script>
+
 <style>
-@import url('https://fonts.googleapis.com/css?family=Chivo:300,400|Playfair+Display:400i,700i&display=swap');
 
 body{
   --color-text: #284135;
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Chivo:300,400|Playfair+Display:400i,700i&display=swap' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
