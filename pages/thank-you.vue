15ed6167fc5cfed4c81b95b92efce9506a3e8a9f
Luca Spagnolo <luca.spagnolo91@gmail.com>
2019-10-08 14:50:32 +0200

    define tag template in render function contact page

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,8 +1,10 @@
-<div class="content">
-  <h1 class="title">
-    Thank you
-  </h1>
-  <p class="text-center">
-    <a href="/">Back to home</a>
-  </p>
-</div>
+<template>
+  <div class="content">
+    <h1 class="title">
+      Thank you
+    </h1>
+    <p class="text-center">
+      <a href="/">Back to home</a>
+    </p>
+  </div>
+</template>
